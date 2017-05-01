0304f07989978ef4fb937c9f11e715150f244ce9
ejdna5 <ejj2@rice.edu>
2017-05-01 00:01:38 -0500

    update

---
+++ app1.py
@@ -0,0 +1,12 @@
+import os
+import flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(port=os.environ.get('PORT', '5000'))
+
